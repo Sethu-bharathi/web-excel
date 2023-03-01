0ab7a24b36761fb917049e3d0e1ea4a66e7007ef
Sethu-bharathi <bharathisethu018@gmail.com>
2023-03-01 11:53:31 +0530

    Completed all features

--- Js/index.js
+++ Js/index.js
@@ -1,17 +1,27 @@
-const user = window.localStorage.getItem("myCat");
 const table = document.getElementById("create");
-const data = JSON.parse(window.localStorage.getItem("data")) ?? {};
-
-if (!data[user]) {
-  data.length = 0;
-  data.width = 0;
-}
+const state = JSON.parse(window.localStorage.getItem("state")) ?? {
+  currentUser: null,
+  users: [],
+  data: {},
+};
+const users=state.users
+const data = state.data;
+let currentUser = state.currentUser;
+console.log(data[currentUser]);
 
 const cell = (row, col) =>
-  `<div contenteditable="true" row="${row}" col="${col}" height="20" width="100" class="cell" focusout="outFocus()">${data[user][row][col]??" "}</div>`;
+  `<div contenteditable="true" row="${row}" col="${col}" height="20" width="100" class="cell" focusout="outFocus()">${
+    data[currentUser].excel[row][col] ?? " "
+  }</div>`;
+
+if (state.currentUser) {
+  createExcel(0, 0, data[currentUser].length, data[currentUser].width);
+} else {
+  showForm();
+}
+
 
 function createExcel(startRow, startCol, row, col) {
-  console.log(data);
   for (let index = 0; index < row; index++) {
     const excelRow = document.createElement("div");
     excelRow.className = "row";
@@ -20,28 +30,45 @@ function createExcel(startRow, startCol, row, col) {
     }
     table.append(excelRow);
   }
+
   // localStorage.setItem("")
 }
 
 async function createGridNM() {
-  // const row = document.getElementById("row").value;
-  // const col = document.getElementById("column").value;
-  const row = 5,
-    col = 5;
-
+  table.innerHTML="";
+  const row = +document.getElementById("row").value;
+  const col = +document.getElementById("column").value;
+  const newUser = document.getElementById("new-user").value;
+  if(users.includes(newUser)){
+    return alert(`User already exists`);
+  }
+  currentUser= newUser;
   if (!row || !column) {
     alert("Fill all the fields");
     return;
   }
+  data[currentUser] = {};
+  data[currentUser].length = 0;
+  data[currentUser].width = 0;
+  data[currentUser].excel = Array.from(Array(row), () => new Array(col));
+  console.log(data[currentUser]);
+  //Creating new excel
   createExcel(0, 0, row, col);
+
   document.querySelector(".main").classList.add("active");
   document.querySelector(".table-container").classList.remove("active");
-  data[user] = Array.from(Array(row), () => new Array(col));
-  (data.length = row - 1), (data.width = col - 1);
+  (data[currentUser].length = row - 1), (data[currentUser].width = col - 1);
+
+  //updating the state
+  state.currentUser = currentUser;
+  state.users = [...state.users, currentUser];
+
+  //storing it in localstorage
+  window.localStorage.setItem("state", JSON.stringify(state));
 }
-if (!window.localStorage.getItem("data")) {
-  createGridNM();
-} else {
+function showForm() {
+  document.querySelector(".excel-form").classList.remove("active");
+  document.querySelector(".table-container").classList.add("active");
 }
 
 function outFocus() {
@@ -49,47 +76,74 @@ function outFocus() {
 }
 
 function addRow() {
-  data[user].push(new Array(data.width));
+  data[currentUser].excel.push(new Array(data.width));
   createExcel(
-    data.length,
-    data.width,
+    data[currentUser].length,
+    data[currentUser].width,
     1,
     document.querySelectorAll(".row")[0]?.children.length ?? 10
   );
-  data.length++;
-  window.localStorage.setItem("data", JSON.stringify(data));
+  data[currentUser].length++;
+  window.localStorage.setItem("state", JSON.stringify(state));
 }
 
 function deleteRow() {
   if (!table.lastChild) return alert("There is no row to delete");
-  data.length--;
+  data[currentUser].length--;
   table.removeChild(table.lastChild);
-  data[user].pop();
-  window.localStorage.setItem("data", JSON.stringify(data));
+  data[currentUser].excel.pop();
+  window.localStorage.setItem("state", JSON.stringify(state));
 }
 function addColumn() {
-  data.width++;
-  data[user].forEach((row) => row.push(""));
+  data[currentUser].width++;
+  data[currentUser].excel.forEach((row) => row.push(""));
   document.querySelectorAll(".row").forEach((row, index) => {
     row.innerHTML += cell(index);
   });
-  window.localStorage.setItem("data", JSON.stringify(data));
+  window.localStorage.setItem("state", JSON.stringify(state));
 }
 function deleteColumn() {
-  data.width--;
+  data[currentUser].width--;
   document.querySelectorAll(".row").forEach((row) => {
     row.removeChild(row.lastChild);
   });
-  data[user].forEach((row) => row.pop());
-  window.localStorage.setItem("data", JSON.stringify(data));
+  data[currentUser].excel.forEach((row) => row.pop());
+  window.localStorage.setItem("state", JSON.stringify(state));
 }
 
 const form = document.getElementById("create");
 form.addEventListener("focusout", (event) => {
   const element = event.target;
   console.log(element.getAttribute("row"), element.getAttribute("col"));
-  data[user][element.getAttribute("row")][element.getAttribute("col")] =
-    element.innerText;
+  data[currentUser].excel[element.getAttribute("row")][
+    element.getAttribute("col")
+  ] = element.innerText;
   console.log(data);
-  window.localStorage.setItem("data", JSON.stringify(data));
+  window.localStorage.setItem("state", JSON.stringify(state));
 });
+
+function getUser() {
+  document.getElementById("user").insertAdjacentHTML(
+    "afterBegin",
+    users.map((user) => {
+      if(user==state.currentUser){
+        return `<option value="${user}" selected>${user}</option>`
+      }
+      return`<option value="${user}">${user}</option>`;
+    })
+  );
+}
+getUser();
+
+function changeHandler(selectedVal) {
+  if (selectedVal.value==="new User"){
+    showForm();
+  };
+  switchUser(selectedVal.value);
+}
+
+function switchUser(nextUser){
+  currentUser=nextUser;
+  table.innerHTML="";
+  createExcel(0,0,data[currentUser].length,data[currentUser].width);
+}
